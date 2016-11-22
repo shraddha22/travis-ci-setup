be4472e132be34f104b007badca0653a3341e526
shraddha22 <shraddha.nayak@celigo.com>
2016-11-22 16:23:34 +0530

    file changes

--- build/scripts/setup_credentials.sh
+++ build/scripts/setup_credentials.sh
@@ -20,8 +20,9 @@ EOF
 mkdir -p $SOL_CREDDIR
 
 cat >$SOL_CREDFILE <<EOF
-username = 'dev_salesforce@sfdc.com'
-password = 'IuZUvgf9mJfH7pS3qG4Dq84hymSlOl0D9dbC'
-token = '7pS3qG4Dq84hymSlOl0D9dbC'
-url = 'https://login.salesforce.com'
+username = $SFDC_USERNAME
+password = $SFDC_PASSWORD
+token = $SFDC_TOKEN
+url = $SFDC_URL
+sf.version = 35.0
 EOF
