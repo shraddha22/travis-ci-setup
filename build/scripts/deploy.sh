338d4e92a00098b15d77bb579bf5c44c650b48a1
shraddha22 <shraddha.nayak@celigo.com>
2016-11-22 19:08:52 +0530

    fix1

--- build/scripts/deploy.sh
+++ build/scripts/deploy.sh
@@ -4,6 +4,6 @@ set -e
 
 SOL_ROOT="$TRAVIS_BUILD_DIR/build/solenopsis/scripts"
 
-cd $SOL_ROOT
-- chmod +x ./solenopsis/scripts/bsolenopsis
-- ./bsolenopsis destructive-push
+cd $SOL_ROOT 
+- - chmod +x ./bsolenopsis
+-./bsolenopsis destructive-push
